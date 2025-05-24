417cc07ee83dfdc85a4fc95a1102dea01b866ddf
Agent Smith <agentsmith7171@gmail.com>
2025-05-24 07:06:04 -0700

    Choco packaging for SchemaQuench 481 and 90 and SchemaTongs 90

---
+++ packaging/chocolatey/SchemaQuench-net9.0/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'SchemaQuench-net9.0.zip'
