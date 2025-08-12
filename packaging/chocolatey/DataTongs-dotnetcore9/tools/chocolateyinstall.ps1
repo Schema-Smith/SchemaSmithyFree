62a70389377c2fbe961e382024c2977c6cf4a718
Agent Smith <agentsmith7171@gmail.com>
2025-08-12 12:40:18 -0700

    Updating chocolatey for 1.1.2

---
+++ packaging/chocolatey/DataTongs-dotnetcore9/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipArchive = Join-Path $toolsDir -ChildPath 'DataTongs-net9.0.zip'
+
+Get-ChocolateyUnzip $zipArchive $toolsDir
+
---
+++ packaging/chocolatey/DataTongs-netfx481/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'DataTongs-net481.zip'
