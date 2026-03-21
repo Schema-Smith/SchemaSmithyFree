61ab488d22e8d56c2057b1b05b30bf4ee6c2cf46
Dr Zachary Smith <dr.zachary.smith68@gmail.com>
2026-03-21 17:17:13 -0400

    fix: remove RTRIM from fn_StripParenWrapping inner loop to match Enterprise

--- Schema/Scripts/SchemaSmith.fn_StripParenWrapping.sql
+++ Schema/Scripts/SchemaSmith.fn_StripParenWrapping.sql
@@ -7,7 +7,7 @@ BEGIN
   WHILE LEFT(RTRIM(@p_Input), 1) = '(' AND RIGHT(RTRIM(@p_Input), 1) = ')' AND NOT @Done = 1
   BEGIN
     DECLARE @Pos INT = 2, @Count INT = 1
-	WHILE @Pos < LEN(RTRIM(@p_Input))
+	WHILE @Pos < LEN(@p_Input)
 	BEGIN
 	  IF SUBSTRING(@p_Input, @Pos, 1) = '(' SET @Count = @Count  + 1
 	  IF SUBSTRING(@p_Input, @Pos, 1) = ')' SET @Count = @Count  - 1
