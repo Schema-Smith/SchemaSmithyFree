6a426aa9e12346bab20bac4b0f44695d40214c8f
Dr Zachary Smith <dr.zachary.smith68@gmail.com>
2026-06-18 08:13:52 -0400

    feat(schema): add delimiter-aware Identifier.SplitQualifiedName + tests (#272)

--- Schema/Schema.UnitTests/Utility/IdentifierTests.cs
+++ Schema/Schema.UnitTests/Utility/IdentifierTests.cs
@@ -83,4 +83,62 @@ public class IdentifierTests
         Assert.That(unwrappedConfig, Is.EqualTo(unwrappedFk),
             "Quote-wrapped source schema config and FK schema must unwrap to the same bare value");
     }
+
+    // ---- SplitQualifiedName (#272: delimiter-aware schema/object split) ----
+
+    [Test]
+    public void Split_SqlServer_FullyBracketed()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("[dbo].[Orders]", Platform.SqlServer);
+        Assert.That(schema, Is.EqualTo("dbo"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_SqlServer_Bare()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("dbo.Orders", Platform.SqlServer);
+        Assert.That(schema, Is.EqualTo("dbo"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_SqlServer_MixedBracketedSchemaBareObject()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("[dbo].Orders", Platform.SqlServer);
+        Assert.That(schema, Is.EqualTo("dbo"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_SqlServer_MixedBareSchemaBracketedObject()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("dbo.[Orders]", Platform.SqlServer);
+        Assert.That(schema, Is.EqualTo("dbo"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_SqlServer_DottedDelimitedSchema_NotMisSplit()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("[my.schema].[Orders]", Platform.SqlServer);
+        Assert.That(schema, Is.EqualTo("my.schema"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_PostgreSql_DottedDelimitedSchema_NotMisSplit()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("\"my.schema\".\"Orders\"", Platform.PostgreSQL);
+        Assert.That(schema, Is.EqualTo("my.schema"));
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
+
+    [Test]
+    public void Split_Unqualified_ReturnsNullSchema()
+    {
+        var (schema, name) = Identifier.SplitQualifiedName("Orders", Platform.SqlServer);
+        Assert.That(schema, Is.Null);
+        Assert.That(name, Is.EqualTo("Orders"));
+    }
 }
