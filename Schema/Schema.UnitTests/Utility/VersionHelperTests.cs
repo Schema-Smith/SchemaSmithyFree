cbcda8920e1fc5abb7322328ca6f9e7bf534eec1
Dr Zachary Smith <dr.zachary.smith68@gmail.com>
2026-06-27 07:14:20 -0400

    test(version): cover 2016 year-alias, dotted + bare declared versions, detected-null

--- Schema/Schema.UnitTests/Utility/VersionHelperTests.cs
+++ Schema/Schema.UnitTests/Utility/VersionHelperTests.cs
@@ -12,10 +12,13 @@ namespace Schema.UnitTests.Utility
         [TestCase("2017", Platform.SqlServer, 14)]
         [TestCase("2019", Platform.SqlServer, 15)]
         [TestCase("2022", Platform.SqlServer, 16)]
+        [TestCase("2016", Platform.SqlServer, 13)]
         [TestCase("15", Platform.SqlServer, 15)]   // already-a-major declaration
         [TestCase("16", Platform.PostgreSQL, 16)]
+        [TestCase("15.3", Platform.PostgreSQL, 15)]
         [TestCase("8.0", Platform.MySQL, 800)]
         [TestCase("8.4", Platform.MySQL, 804)]
+        [TestCase("8", Platform.MySQL, 800)]
         public void ParseDeclaredVersion_NormalizesToComparable(string version, Platform platform, int expected)
         {
             Assert.That(VersionHelper.ParseDeclaredVersion(version, platform), Is.EqualTo(expected));
@@ -26,6 +29,13 @@ namespace Schema.UnitTests.Utility
         public void ParseDeclaredVersion_ReturnsNull_WhenUnparseable(string version, Platform platform)
         {
             Assert.That(VersionHelper.ParseDeclaredVersion(version, platform), Is.Null);
+        }
+
+        [TestCase("not-a-version", Platform.PostgreSQL)]
+        [TestCase("", Platform.MySQL)]
+        public void ParseDetectedVersion_ReturnsNull_WhenUnparseable(string raw, Platform platform)
+        {
+            Assert.That(VersionHelper.ParseDetectedVersion(raw, platform), Is.Null);
         }
 
         [TestCase("160004", Platform.PostgreSQL, 16)]
